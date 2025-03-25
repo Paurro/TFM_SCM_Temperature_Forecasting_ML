4a3b8b5eeb207aadc95427d334e8f1942d5c360c
Paurro <pau.rodrigo.14@gmail.com>
2025-03-25 09:41:38 +0100

    Canvis manera de baixar arxius i dades 25-03-2025

--- casos_previs/forecast_markov/Download_data.py
+++ casos_previs/forecast_markov/Download_data.py
@@ -1,7 +1,8 @@
 ### SCRIPT PER DESCARREGAR DADES DE LA WEB DE LA SCM I GUARDAR-LES EN UN FITXER ###
 
+#Guarda totes les dades en un fitxer JSON
 
-# Descarrega de les dades de tempertatura
+# Descarrega de les dades de temperatura
 
 #################### LLIBRERIES ####################
 
@@ -15,13 +16,12 @@ import numpy as np
 import pandas as pd
 
 
-
 #################### CREAR DIRECTORIS ####################
 
 # Estació que es baixarà les dades i dates que es volen baixar
 # Paràmetres de la petició
 
-data_inici = '1999-01-01'
+data_inici = '1998-01-01' 
 data_final = '2024-12-31'
 est = 'Z1' # Bonaigua
 var = 'T'
@@ -46,7 +46,6 @@ if not os.path.exists(data_path):
     os.makedirs(data_path, exist_ok=True)
 
 
-
     ######## Carpeta i fitxers metadades
 
 # Creem la carpeta on posarem les metadades de l'estació i variable
@@ -67,7 +66,6 @@ file_path_meta = os.path.join(meta_data_path, file_name_meta)
 url_meta = f'http://smcawrest01:8080/ApiRestInterna/xema/v1/mesurades/metadades/estacions/{est}/variables/{codi_var}?xarxa=1'
 
 
-
 #################### DESCARREGA DE LES METADADES ####################
 
 if not os.path.exists(file_path_meta):
@@ -76,11 +74,9 @@ if not os.path.exists(file_path_meta):
     print(f'Descarregant metadades \n')
 
     # Fem la consulta
-
     response = requests.get(url_meta)
 
     # Si la resposta es correcte, guardem les dades en un fitxer
-
     if response.status_code == 200:
         
         print(f'METADATES Descarregades correctament \n')
@@ -90,7 +86,6 @@ if not os.path.exists(file_path_meta):
             json.dump(response.json(), file, indent=4)
 
     else:
-
         error = json.loads(response.text)
         error_message = error['message']
         print(f' Error consultant metadades --> {error_message} \n')
@@ -102,62 +97,47 @@ else:
     print('-----------------------------------')
 
 
-
-
-
-
 #################### DESCARREGA DE LES DADES ####################
 
-for date in pd.date_range(start=data_inici, end=data_final, freq='D'):
+# Nom del fitxer on guardarem les dades
+file_name = f'SCM_{var}_{est}.json'
 
-    # Data en format YYYYMMDD per el nom del fitxer
-    data_f = date.strftime('%Y%m%d')
+# Ruta on guardarem el fitxer
+file_path = os.path.join(data_path, file_name)
 
-    # Nom del fitxer on guardarem les dades
-    file_name = f'SCM_{est}_T_{data_f}.json'
+# Si el fitxer no existeix, el creem i descarreguem les dades
+if not os.path.exists(file_path):
+    print('-----------------------------------')
+    print(f'Descarregant dades de estació {est} i variable {var} de la data {data_inici} a {data_final} \n')
+    print('-----------------------------------')
 
-    di = date.strftime('%Y-%m-%dT00:00Z').replace(':','%3A')
-    df = date.strftime('%Y-%m-%dT23:59Z').replace(':','%3A')
-
-
-    # Exemple de URL per descarregar les dades de temperatura de l'estació Bonaigua per al dia 1998-01-01 de 00:00 a 23:59
-    # 'http://smcawrest01:8080/ApiRestInterna/xema/v1/mesurades/dades/estacions/Z1/variables/32?din=1998-01-01T00%3A00Z&dfi=1998-01-01T23%3A59Z&xarxa=1&nv=false'
-      
-
-    url_data = f'http://smcawrest01:8080/ApiRestInterna/xema/v1/mesurades/dades/estacions/{est}/variables/{codi_var}?din={di}&dfi={df}&xarxa=1&nv=false'
-
+    dades = []  # Llista per emmagatzemar totes les dades
     
-    # Ruta on guardarem el fitxer
-    file_path = os.path.join(data_path, file_name)
+    for date in pd.date_range(start=data_inici, end=data_final, freq='D'):
+        di = date.strftime('%Y-%m-%dT00:00Z').replace(':','%3A')
+        df = date.strftime('%Y-%m-%dT23:59Z').replace(':','%3A')
 
-    # Si el fitxer no existeix, descarreguem les dades
-
-    if not os.path.exists(file_path):
-        
-        print(f'Descarregant dades de la data {date} \n')
+        url_data = f'http://smcawrest01:8080/ApiRestInterna/xema/v1/mesurades/dades/estacions/{est}/variables/{codi_var}?din={di}&dfi={df}&xarxa=1&nv=false'
 
         # Fem la consulta
         response = requests.get(url_data)
-
-        # Si la resposta es correcte, guardem les dades en un fitxer
-
+    
         if response.status_code == 200:
-
-            # Guardem les dades en un fitxer json
-            with open(file_path, 'w') as file:
-                json.dump(response.json(), file, indent=4)
-
+            dades.append(response.json())  # Afegim les dades a la llista
         else:
-
             error = json.loads(response.text)
             error_message = error['message']
-            print(f' Error consultant dades --> {error_message} \n')
+            print(f' Error consultant dades {date.strftime("%Y-%m-%d")}: {error_message} \n')
             print('-----------------------------------')
+    
+    # Guardem totes les dades en un sol fitxer JSON
+    with open(file_path, 'w') as file:
+        json.dump(dades, file, indent=4)
 
-    else:
-        print(f'Dades de la data {date} ja descarregades \n')
-        print('-----------------------------------')
+    print('Descarrega de dades finalitzada \n')
+    print('-----------------------------------')
 
-print('Descarrega de dades finalitzada \n')
-print('-----------------------------------')
-
+else: # Si el fitxer ja existeix
+    print('-----------------------------------')
+    print(f'Dades ja descarregades i/o fitxer ja creat \n')
+    print('-----------------------------------')
